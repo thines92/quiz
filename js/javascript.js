4cf433af6ccb00da72adc8e223bbb7bd1a8acb08
Tyler <tylerhines1992@gmail.com>
2015-10-28 22:40:54 -0400

    added javascript

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,11 @@
 /**
  * Created by tyler on 10/28/15.
  */
+
+var allQuestions = [{question1: "What is 1 + 1?", choices:["1", "2", "3", "4"]}];
+
+//alert(JSON.stringify(allQuestions[0].choices[0]));
+$("#submitButton").click(function() {
+    $("#question1").val(1);
+    $("#question1").text("Hello There");
+})
