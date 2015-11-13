937a0b73ab7c38f0e23bf6b635db3738d4d4cfa4
tyler <tylerhines1992>
2015-11-13 14:54:56 -0500

    Add array shuffle function to determine question

--- js/javascript.js
+++ js/javascript.js
@@ -6,32 +6,55 @@ var allQuestions = [{question1: "What is 1 + 1?", choices: [ "1", "2", "3", "4" 
   {question2: "What is 2 + 2?", choices: ["6", "2", "3", "4",]},
   {question3: "What is 3 + 3?", choices: ["3", "6", "9", "12"]}];
 
- function getRandomNumber() {
-   randomNumber = Math.floor(Math.random() * 2) + 1;
+var newArray = shuffleArray(allQuestions);
+alert(newArray);
 
-   return randomNumber;
- }
+  function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+ //
+ // function getRandomNumber() {
+ //   randomNumber = Math.floor(Math.random() * 2) + 1;
+ //
+ //   return randomNumber;
+ // }
+ //
+ // function isNumberDuplicate(randomNumber, tempNumber) {
+ //   if (randomNumber == tempNumber) {
+ //     getRandomNumber();
+ //   }
+ // }
 
- function isNumberDuplicate(randomNumber, tempNumber) {
-   if (randomNumber == tempNumber) {
-     getRandomNumber();
-   }
- }
-
- function appendQuestions(randomNumber) {
-   $("#questionList").append("<input id='question1' type='radio'>" +
-    JSON.stringify(allQuestions[tempNumber - 1].choices[0]) + "</input>")
-   $("#questionList").append("<input id='question2' type='radio'>" +
-    JSON.stringify(allQuestions[tempNumber - 1].choices[1]) + "</input>")
-   $("#questionList").append("<input id='question3' type='radio'>" +
-    JSON.stringify(allQuestions[tempNumber - 1].choices[2]) + "</input>")
-   $("#questionList").append("<input id='question4' type='radio'>" +
-    JSON.stringify(allQuestions[tempNumber - 1].choices[3]) + "</input>")
+ // function increaseByOne() {
+ //   number = 0;
+ //   number++;
+ // }
+ function appendQuestions(number) {
+    if (newArray == "undefined" || newArray == "null" || newArray.length == 0) {
+      document.getElementById("questionList").innerHTML = "Complete!";
+    } else {
+     $("#questionList").append("<input id='question1' type='radio'>" +
+      JSON.stringify(newArray[0].choices[0]) + "</input>")
+     $("#questionList").append("<input id='question2' type='radio'>" +
+      JSON.stringify(newArray[0].choices[1]) + "</input>")
+     $("#questionList").append("<input id='question3' type='radio'>" +
+      JSON.stringify(newArray[0].choices[2]) + "</input>")
+     $("#questionList").append("<input id='question4' type='radio'>" +
+      JSON.stringify(newArray[0].choices[3]) + "</input>")
+    }
  }
 
 $("#submitButton").click(function () {
   $("#questionList").empty();
-  tempNumber = getRandomNumber();
-  isNumberDuplicate();
+  // tempNumber = getRandomNumber();
+  // isNumberDuplicate();
+  // increaseByOne();
   appendQuestions();
+  newArray.shift();
 })
