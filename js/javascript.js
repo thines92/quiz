c5f768c26d2430d3e33945e772e0d1deba027444
tyler <tylerhines1992>
2015-11-13 13:46:11 -0500

    Add code to select a random number between 0 and 1

--- js/javascript.js
+++ js/javascript.js
@@ -2,14 +2,14 @@
  * Created by tyler on 10/28/15.
  */
 
-var allQuestions = [{question1: "What is 1 + 1?", choices: [ "1", "2", "3", "4" ]}];
+var allQuestions = [{question1: "What is 1 + 1?", choices: [ "1", "2", "3", "4" ]},
+ {question2: "What is 2 + 2?", choices: ["6", "2", "3", "4",]}];
 
-//alert(JSON.stringify(allQuestions[0].choices[0]));
 $("#submitButton").click(function () {
-    // $("#question1").val(1);
-    // $("#question1").text("Hello There");
-    $("#questionLabel").append("<input id='question1' type='radio'>" + JSON.stringify(allQuestions[0].choices[0]) + "</input>")
-    $("#questionLabel").append("<input id='question1' type='radio'>" + JSON.stringify(allQuestions[0].choices[1]) + "</input>")
-    $("#questionLabel").append("<input id='question1' type='radio'>" + JSON.stringify(allQuestions[0].choices[2]) + "</input>")
-    $("#questionLabel").append("<input id='question1' type='radio'>" + JSON.stringify(allQuestions[0].choices[3]) + "</input>")
+  $("#questionList").empty();
+  var randomNumber = Math.floor(Math.random() * 2) + 1;
+  $("#questionList").append("<input id='question1' type='radio'>" + JSON.stringify(allQuestions[randomNumber - 1].choices[0]) + "</input>")
+  $("#questionList").append("<input id='question2' type='radio'>" + JSON.stringify(allQuestions[randomNumber - 1].choices[1]) + "</input>")
+  $("#questionList").append("<input id='question3' type='radio'>" + JSON.stringify(allQuestions[randomNumber - 1].choices[2]) + "</input>")
+  $("#questionList").append("<input id='question4' type='radio'>" + JSON.stringify(allQuestions[randomNumber - 1].choices[3]) + "</input>")
 })
